15a4f653261eafb4bbabacad3407ec02e41a5be5
sardoralgoritm <sardorsaminov5@gmail.com>
2024-01-10 20:59:17 +0500

    Add project files.

---
+++ TodoProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TodoProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
